4739fc5366f221208e4ec7574c8da4552697f01e
michaelamando1 <michaelamando25@gmail.com>
2024-04-13 09:11:07 +0100

    first commit

---
+++ variables.js
@@ -0,0 +1,15 @@
+let a = 3;
+
+let b = 3;
+
+if (a == b) {
+    console.log("A is equals to B")
+} else {
+    console.log("A is not equals to B")
+}
+
+let name1 = "Michael"
+
+let name2 = "Okoh"
+
+console.log(name1 + name2)
